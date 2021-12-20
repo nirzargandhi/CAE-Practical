51b8ef28fd08bcd484d5c8d63fd512d863573ea5
Nirzar <54696036+nirzargandhi@users.noreply.github.com>
2021-12-20 09:59:23 +0530

    1.Project created.

---
+++ CAE-PracticalUITests/CAE_PracticalUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CAE_PracticalUITestsLaunchTests.swift
+//  CAE-PracticalUITests
+//
+//  Created by Nirzar Gandhi on 20/12/21.
+//
+
+import XCTest
+
+class CAE_PracticalUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
